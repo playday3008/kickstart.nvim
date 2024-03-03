8bc219b18272d00991f9e607a93128df9b84743b
PlayDay <18056374+playday3008@users.noreply.github.com>
2024-03-03 12:49:00 +0100

    Update defaults.lua

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,4 +1,1 @@
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undodir"
